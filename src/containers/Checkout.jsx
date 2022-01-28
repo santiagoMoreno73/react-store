a9fc8978afd6dbec1c8759fb935e35e8bc41e6cb
santiagoMoreno73 <santimor2009@hotmail.com>
2022-01-28 13:22:47 -0500

    Add Form checkout and Information

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+// css
+import "../styles/components/Checkout.css";
+
 const Checkout = () => {
   return (
-    <div>
-      <h1>Checkout</h1>
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de Pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>$18</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
     </div>
   );
 };
