54cb6392c1e1c4510ea0b8353be909ca68798a89
santiagoMoreno73 <santimor2009@hotmail.com>
2022-02-09 10:17:57 -0500

    [add] firebase config

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 
 // components
 import Product from "./Product";
+import Loader from "./Shared/Loader";
 
 //context
 import AppContext from "../context/AppContext";
@@ -15,6 +16,10 @@ import "../styles/components/Products.css";
 const Products = () => {
   const { state, addToCart } = useContext(AppContext);
   const { products } = state;
+
+  if (products.length == 0) {
+    return <Loader />;
+  }
 
   const handleAddToCart = (product) => () => {
     const idRandom = Math.floor(Math.random() * 1000);
