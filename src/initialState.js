fcaa2f729f6708385bf2071f0f7f180e126719a2
santiagoMoreno73 <santimor2009@hotmail.com>
2022-02-13 02:46:20 -0500

    [add]: images products

--- src/initialState.js
+++ src/initialState.js
@@ -5,43 +5,49 @@ export default {
   products: [
     {
       id: "1",
-      image: "https://arepa.s3.amazonaws.com/camiseta.png",
-      title: "Camiseta",
+      image:
+        "https://micro-frontends-es.org/1-composition-client-only/team-red/images/tractor-red.jpg",
+      title: "Porsche-Diesel 419",
       price: 25,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
     {
       id: "3",
-      image: "https://arepa.s3.amazonaws.com/mug.png",
-      title: "Mug",
-      price: 10,
+      image:
+        "https://micro-frontends-es.org/1-composition-client-only/team-green/images/reco_8.jpg",
+      title: "Ferrari Testa",
+      price: 40,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
     {
       id: "4",
-      image: "https://arepa.s3.amazonaws.com/pin.png",
-      title: "Pin",
-      price: 4,
+      image:
+        "https://micro-frontends-es.org/1-composition-client-only/team-green/images/reco_3.jpg",
+      title: "Remolque",
+      price: 8,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
     {
       id: "5",
-      image: "https://arepa.s3.amazonaws.com/stickers1.png",
-      title: "Stickers",
-      price: 2,
+      image:
+        "https://micro-frontends-es.org/1-composition-client-only/team-green/images/reco_2.jpg",
+      title: "Ford Classic Two",
+      price: 25,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
     {
       id: "6",
-      image: "https://arepa.s3.amazonaws.com/stickers2.png",
-      title: "Stickers",
-      price: 2,
+      image:
+        "https://micro-frontends-es.org/1-composition-client-only/team-green/images/reco_1.jpg",
+      title: "Volkswagen",
+      price: 28,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
     {
       id: "7",
-      image: "https://arepa.s3.amazonaws.com/hoodie.png",
-      title: "Hoodie",
+      image:
+        "https://micro-frontends-es.org/1-composition-client-only/team-red/images/tractor-green.jpg",
+      title: "Eicher Diesel 215/16",
       price: 35,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     },
